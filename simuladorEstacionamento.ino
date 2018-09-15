8ac0a8ca0620a1d6a210232548a6ec9172cb6cf5
Lucas Andrade <lap.andrade15@gmail.com>
2018-09-14 22:44:19 -0300

    example

--- simuladorEstacionamento.ino
+++ simuladorEstacionamento.ino
@@ -1,3 +1,17 @@
+#include <Fuzzy.h>
+#include <FuzzyComposition.h>
+#include <FuzzyInput.h>
+#include <FuzzyIO.h>
+#include <FuzzyOutput.h>
+#include <FuzzyRule.h>
+#include <FuzzyRuleAntecedent.h>
+#include <FuzzyRuleConsequent.h>
+#include <FuzzySet.h>
+
+//Constante que representa a distância fica em que o veículo é deslocado a cada passo.
+//Defibir melhor valor posteriormente.
+const int w = 5;
+
 void setup() {
   // put your setup code here, to run once:
 
