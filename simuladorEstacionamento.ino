bec2c57aa1ab5f67803e0856bc2f1418904894ed
Lucas Andrade <lap.andrade15@gmail.com>
2018-09-14 23:00:20 -0300

    add

--- simuladorEstacionamento.ino
+++ simuladorEstacionamento.ino
@@ -12,8 +12,29 @@
 //Defibir melhor valor posteriormente.
 const int w = 5;
 
+//Instanciando um objeto da biblioteca
+Fuzzy* fuzzy = new Fuzzy();
+
 void setup() {
-  // put your setup code here, to run once:
+  //Iniciando comunicação serial
+  Serial.begin(9600);
+
+  //Criando um FuzzyInput de entrada
+  //Posição X = Posição do veículo em relação ao eixo horizontal X
+  FuzzyInput* posicaoX = new FuzzyInput(1);
+
+  //Partições nebulosas: LateralEsquerda, CentroEsqueda, Centro, CentroDireita, Direita
+
+  FuzzySet* lateralEsquerda = new FuzzySet(0,0,10,35);
+  posicaoX->addFuzzySet(lateralEsquerda);
+  FuzzySet* centroEsquerda = new FuzzySet(30,40,40,50);
+  posicaoX->addFuzzySet(centroEsquerda);
+  FuzzySet* centro = new FuzzySet(45,50,50,55);
+  posicaoX->addFuzzySet(centro);
+  FuzzySet* centroDireita = new FuzzySet(50,60,60,70);
+  posicaoX->addFuzzySet(centroDireita);
+  FuzzySet* direita = new FuzzySet(65,90,100,100);
+  posicaoX->addFuzzySet(direita);
 
 }
 
